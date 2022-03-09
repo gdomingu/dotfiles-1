931eaa4a9c907c6d5dca2907ad264bd172c88d55
Gabe D <gdomingu@github.com>
2022-03-08 17:04:31 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ cd $DOTFILESDIRREL/..
 DOTFILESDIR="$(pwd -P)"
 
 ln -sf "$DOTFILESDIR" "$HOME/.dotfiles"
-cat "$DOTFILESDIR/.zprofile " >> "$HOME/.zprofile "
+cat "$DOTFILESDIR/shell/.zprofile " >> "$HOME/.zprofile "
